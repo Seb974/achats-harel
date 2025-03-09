916ab8d061518219e78beb9a6399e9791c8b87bf
Seb97_4 <m_seb@icloud.com>
2025-03-09 23:05:02 +0400

    circuit list optimisation

--- pwa/components/admin/circuit/CircuitsList.tsx
+++ pwa/components/admin/circuit/CircuitsList.tsx
@@ -1,28 +1,17 @@
 import { type NextPage } from "next";
 import {
-  TextInput,
   Datagrid,
-  useRecordContext,
   List,
   TextField,
-  WrapperField,
   CreateButton,
-  DatagridConfigurable,
   ExportButton,
   FilterButton,
-  SelectColumnsButton,
   TopToolbar,
   DateField,
   NumberField,
-  DateInput,
   EditButton,
   ShowButton
-
-  
 } from "react-admin";
-
-// import { useRouter } from "next/navigation";
-// import { useMutation } from "@tanstack/react-query";
 import { useMercure } from "../../../utils/mercure";
 import { type Circuit } from "../../../types/Circuit";
 import { type PagedCollection } from "../../../types/collection";
@@ -41,29 +30,11 @@ const ListActions = () => (
   </TopToolbar>
 );
 
-const filters = [
-  // <TextInput source="prestation.aeronef.immatriculation" key="Aeronef" label="Aéronef"/>,
-  <TextInput source="prestation.pilote.firstName" key="Pilote" label="Pilote" />,
-  <TextInput source="circuit.code" key="Circuit" label="Circuit" />,
-  <DateInput source="prestation.date[after]"  key="DateMin" label="Date Min"/>,
-  <DateInput source="prestationdate[before]"  key="DateMax" label="Date Max"/>,
-];
-
 export const CircuitsList: NextPage<Props> = ({ data, hubURL, page }) => {
   const collection = useMercure(data, hubURL);
-  // const router = useRouter();
-
-  // const filtersMutation = useMutation({
-  //   mutationFn: async (filters: FiltersProps) => {
-  //     router.push(buildUriFromFilters("/books", filters));
-  //   },
-  //   onError: (error) => {
-  //     console.error(error);
-  //   },
-  // });
 
   return (
-    <List resource="circuits" actions={<ListActions/>} filters={ filters }>   {/* filter={{ authorId }} */} 
+    <List resource="circuits" actions={<ListActions/>}>
         <Datagrid>
             <TextField source="code" label="Code" sortable={ true }/>
             <TextField source="nom" label="Nom" sortable={ true }/>
