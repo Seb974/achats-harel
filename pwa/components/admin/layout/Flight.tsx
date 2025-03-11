e7dd400e8273ed62d4580aa1b6389b39bab4dde9
Seb97_4 <m_seb@icloud.com>
2025-03-11 18:41:20 +0400

    Link to prestation create debug

--- pwa/components/admin/layout/Flight.tsx
+++ pwa/components/admin/layout/Flight.tsx
@@ -1,40 +1,26 @@
 import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
 import { ForwardedRef, forwardRef } from "react";
-import { LogoutClasses, useTranslate } from "react-admin";
+import { LogoutClasses } from "react-admin";
 
 import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
-import Link from 'next/link';
-import { redirect } from 'next/navigation'
-
-import { useRouter } from 'next/navigation'
-
-// import ExitIcon from "@mui/icons-material/PowerSettingsNew";
-// import { signOut, useSession } from "next-auth/react";
-
-// import { NEXT_PUBLIC_OIDC_SERVER_URL } from "../../../config/keycloak";
+import { Link } from 'react-router-dom';
 
 const Flight = forwardRef((props, ref: ForwardedRef<any>) => {
-
-    // const router = useRouter();
-
-    // const handleClick = () => redirect(`/admin#/prestations/create`);
 
     return (
         <MenuItem
         className="logout"
-        // onClick={handleClick}
-        // ref={ref}
         component="li"
         {...props}
         >
-        <ListItemIcon className={LogoutClasses.icon}>
-            <Link href="/admin#/prestations/create">
-                <AirplaneTicketIcon fontSize="small" />
+            <Link to="/prestations/create" className="flex">       
+                <ListItemIcon className={LogoutClasses.icon}>
+                        <AirplaneTicketIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>
+                    Enregistrer un vol
+                </ListItemText>
             </Link>
-        </ListItemIcon>
-        <ListItemText>
-            Enregistrer un vol
-        </ListItemText>
         </MenuItem>
     );
     });
