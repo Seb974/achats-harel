e1f6dbf9861468b7d6a4a4bfb9528125157834a7
Seb97_4 <m_seb@icloud.com>
2025-04-23 12:31:10 +0400

    Create reservation link added to appBar

--- pwa/components/admin/layout/AppBar.tsx
+++ pwa/components/admin/layout/AppBar.tsx
@@ -2,32 +2,43 @@ import { AppBar, UserMenu, TitlePortal } from "react-admin";
 import Logout from "./Logout";
 import Flight from "./Flight";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 import { Link } from 'react-router-dom';
 import logo from "../../../public/api-platform/logo.png";
+import Reservation from "./Reservation";
+import { isDefined } from "../../../app/lib/utils";
 
-const CustomAppBar = () => (
-  <AppBar
-    color="error"
-    userMenu={
-      <UserMenu>
-        <Flight />
-        <Logout />
-      </UserMenu>
-    }
-  >
-    <TitlePortal />
-    <div className="flex-1">
-      <Link to="/">
-        <Image
-          alt="Planetair974"
-          src={logo}
-          width={60}
-          height={60}
-        />
-      </Link>
-      
-    </div>
-  </AppBar>
-);
+const CustomAppBar = () => {
+
+  const session = useSession();
+  const user = session.data.user;
+
+  return (
+    <AppBar
+      color="error"
+      userMenu={
+        <UserMenu>
+          {/* @ts-ignore  */}
+          { isDefined(session) && isDefined(user) &&  user.roles.find(r => r === "admin") && <Reservation /> }
+          <Flight />
+          <Logout />
+        </UserMenu>
+      }
+    >
+      <TitlePortal />
+      <div className="flex-1">
+        <Link to="/">
+          <Image
+            alt="Planetair974"
+            src={logo}
+            width={60}
+            height={60}
+          />
+        </Link>
+        
+      </div>
+    </AppBar>
+  )
+};
 
 export default CustomAppBar;
