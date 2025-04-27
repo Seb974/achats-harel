1e0f5eccf2308c97fb402ffe75090cb1d4041b71
Seb97_4 <m_seb@icloud.com>
2025-04-27 21:20:36 +0400

    Defautl value of next intevervention set up on Entretien creation

--- pwa/components/admin/entretien/EntretiensCreate.tsx
+++ pwa/components/admin/entretien/EntretiensCreate.tsx
@@ -5,19 +5,64 @@ import {
   NumberInput,
   DateInput,
   BooleanInput,
+  useDataProvider,
   ReferenceArrayInput
 } from "react-admin";
 import { Create } from "react-admin";
+import { useEffect, useState } from "react";
+import { useWatch, useFormContext } from 'react-hook-form';
+import { isDefined } from "../../../app/lib/utils";
 
-export const EntretiensCreate = () => (
-<Create redirect="list">
-  <SimpleForm>
-    <DateInput source="date" defaultValue={ new Date() } label="Date"/>
-    <ReferenceInput reference="aeronefs" source="aeronef" label="Aéronef" />
-    <ReferenceArrayInput source="intervenants" reference="users" />
-    <TextInput source="intervention" label="Détail de l'intervention" multiline sx={{ '& .MuiInputBase-inputMultiline': {height: '200px!important'} }}/>
-    <BooleanInput source="changementMoteur" label="Changement du moteur" defaultValue={ false }/>
-    <NumberInput source="horametreNextIntervention" label="Prochaine intervention"/>
-  </SimpleForm>
-</Create>
-);
+const AeronefWatcher = ({ setSelectedAeronef, setIsChangementMoteur }) => {
+  const dataProvider = useDataProvider();
+  const { control, setValue } = useFormContext();
+  const [aeronefs, setAeronefs] = useState([]);
+
+  const aeronef = useWatch({ control, name: 'aeronef', defaultValue: null });
+  const changementMoteur = useWatch({ control, name: 'changementMoteur', defaultValue: false});
+
+  useEffect(() => getAeronefs(), []);
+  useEffect(() => setIsChangementMoteur(changementMoteur), [changementMoteur, setIsChangementMoteur]);
+
+  useEffect(() => {
+    if (isDefined(aeronef)) {
+      const selection = aeronefs.find(a => a['@id'] === (typeof aeronef === 'string' ? aeronef : aeronef['@id']));
+      setSelectedAeronef(selection);
+
+      if (selection && selection.id) {
+        const nextValue = !changementMoteur ? selection.entretien : selection.changementMoteur;
+        setValue('horametreNextIntervention', nextValue ?? '');
+      }
+      return;
+    }
+    setSelectedAeronef(aeronef);
+  }, [aeronef, changementMoteur, aeronefs, setSelectedAeronef, setValue]);
+
+  const getAeronefs = () => {
+    dataProvider
+        .getList("aeronefs", {})
+        .then(({ data }) => setAeronefs(data));
+  };
+
+  return null;
+};
+
+export const EntretiensCreate = () => {
+
+  const [selectedAeronef, setSelectedAeronef] = useState(null);
+  const [isChangementMoteur, setIsChangementMoteur] = useState(null);
+
+  return (
+    <Create redirect="list">
+      <SimpleForm>
+        <DateInput source="date" defaultValue={ new Date() } label="Date"/>
+        <ReferenceInput reference="aeronefs" source="aeronef" label="Aéronef" />
+        <ReferenceArrayInput source="intervenants" reference="users" />
+        <TextInput source="intervention" label="Détail de l'intervention" multiline sx={{ '& .MuiInputBase-inputMultiline': {height: '200px!important'} }}/>
+        <BooleanInput source="changementMoteur" label="Changement du moteur" defaultValue={ false }/>
+        <NumberInput key={ isDefined(selectedAeronef) ? selectedAeronef.id : 0 } source="horametreNextIntervention" label="Prochaine intervention"/>
+        <AeronefWatcher setSelectedAeronef={ setSelectedAeronef } setIsChangementMoteur={ setIsChangementMoteur }/>
+      </SimpleForm>
+    </Create>
+  );
+};
