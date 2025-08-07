596847ae2a7f0c128ed6ab8039db0b1753270234
Seb97_4 <m_seb@icloud.com>
2025-08-07 19:23:42 +0400

    Debug wix controller webhook

--- api/src/Controller/WixController.php
+++ api/src/Controller/WixController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use App\Service\DynamicMailerFactory;
 use App\Service\ClientGetter;
 use Symfony\Component\Mime\Email;
 
-class WixController
+class WixController extends AbstractController
 {
     private string $webhookSecret;
     private DynamicMailerFactory $mailerFactory;
