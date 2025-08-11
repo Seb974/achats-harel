ea9b4ec02975b33a57bd69a39890abca093430d6
Seb97_4 <m_seb@icloud.com>
2025-08-11 13:54:35 +0400

    debug end date calculation on prepayment edition and creation

--- pwa/components/admin/cadeau/DateExpirationInput.jsx
+++ pwa/components/admin/cadeau/DateExpirationInput.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useFormContext, useWatch } from 'react-hook-form';
 import { DateInput } from "react-admin";
-import { addDays, addYears } from "date-fns";
 
 export const DateExpirationInput = () => {
   const { setValue } = useFormContext();
@@ -10,9 +9,15 @@ export const DateExpirationInput = () => {
   useEffect(() => {
     if (!dateAchat) return;
 
-    const dateExp = addDays(addYears(new Date(dateAchat), 1), 1);
-    setValue("fin", dateExp);
+    const dateObj = new Date(dateAchat);
+    // Ajoute 1 an
+    dateObj.setFullYear(dateObj.getFullYear() + 1);
+    // Ajoute 1 jour
+    dateObj.setDate(dateObj.getDate() + 1);
+
+    setValue("fin", dateObj);
   }, [dateAchat, setValue]);
 
-  return <DateInput source="fin" label="Date d'expiration" readOnly/>
+  return <DateInput source="fin" label="Date d'expiration" readOnly />
 };
+
