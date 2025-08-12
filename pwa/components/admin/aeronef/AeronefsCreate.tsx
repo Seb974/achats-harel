a95679cb920e5bbe8407f9dd7e8a5915815a718f
Seb97_4 <m_seb@icloud.com>
2025-08-12 19:08:33 +0400

    microtrak input on aircraft form conditioned to client tag option

--- pwa/components/admin/aeronef/AeronefsCreate.tsx
+++ pwa/components/admin/aeronef/AeronefsCreate.tsx
@@ -1,25 +1,31 @@
-import { 
-  SimpleForm, 
-  TextInput,
-  NumberInput,
-  BooleanInput,
-  required
-} from "react-admin";
+import { SimpleForm, TextInput, NumberInput, BooleanInput, required } from "react-admin";
 import { Create } from "react-admin";
+import { useClient } from '../../admin/ClientProvider';
+import { clientWithMicrotrakTags } from "../../../app/lib/client";
 
-export const AeronefsCreate = () => (
-<Create>
-  <SimpleForm>
-    <TextInput source="immatriculation" label="Immatriculation" validate={required()}/>
-    <NumberInput source="horametre" label="Horamètre actuel" validate={required()}/>
-    <NumberInput source="entretien" label="Prochain entretien" validate={required()}/>
-    <NumberInput source="changementMoteur" label="Changement du moteur" />
-    <NumberInput source="seuilAlerte" label="Seuil d'alerte (en h) avant entretien" defaultValue={ 10 } validate={required()}/>
-    <NumberInput source="seuilAlerteChangementMoteur" label="Seuil d'alerte (en h) avant changement du moteur" defaultValue={ 200 }/>
-    <TextInput source="codeBalise" label="Code Microtrak"/>
-    <BooleanInput source="decimal" label="Horamètre décimal"/>
-    { false && <BooleanInput source="alerteEnvoyee" label="Alerte envoyée" defaultValue={ false } /> }
-    { false && <BooleanInput source="alerteMoteurEnvoyee" label="Alerte moteur envoyée" defaultValue={ false } /> }
-  </SimpleForm>
-</Create>
-);
+export const AeronefsCreate = () => {
+
+  const { client } = useClient();
+
+  const MicrotrakInput = () => {
+    return !clientWithMicrotrakTags(client) ? null : 
+      <TextInput source="codeBalise" label="Code Microtrak"/>
+  };
+
+  return (
+    <Create>
+      <SimpleForm>
+        <TextInput source="immatriculation" label="Immatriculation" validate={required()}/>
+        <NumberInput source="horametre" label="Horamètre actuel" validate={required()}/>
+        <NumberInput source="entretien" label="Prochain entretien" validate={required()}/>
+        <NumberInput source="changementMoteur" label="Changement du moteur" />
+        <NumberInput source="seuilAlerte" label="Seuil d'alerte (en h) avant entretien" defaultValue={ 10 } validate={required()}/>
+        <NumberInput source="seuilAlerteChangementMoteur" label="Seuil d'alerte (en h) avant changement du moteur" defaultValue={ 200 }/>
+        <MicrotrakInput/>
+        <BooleanInput source="decimal" label="Horamètre décimal"/>
+        { false && <BooleanInput source="alerteEnvoyee" label="Alerte envoyée" defaultValue={ false } /> }
+        { false && <BooleanInput source="alerteMoteurEnvoyee" label="Alerte moteur envoyée" defaultValue={ false } /> }
+      </SimpleForm>
+    </Create>
+  );
+};
--- pwa/components/admin/aeronef/AeronefsEdit.tsx
+++ pwa/components/admin/aeronef/AeronefsEdit.tsx
@@ -1,25 +1,28 @@
-import { 
-  Edit,
-  SimpleForm, 
-  TextInput,
-  NumberInput,
-  BooleanInput
-} from "react-admin";
+import { Edit, SimpleForm, TextInput, NumberInput, BooleanInput } from "react-admin";
+import { useClient } from "../ClientProvider";
+import { clientWithMicrotrakTags } from "../../../app/lib/client";
 
 export const AeronefsEdit = () => {
 
-  return (
-  <Edit >
-      <SimpleForm>
-        <TextInput source="immatriculation" label="Immatriculation"/>
-        <NumberInput source="horametre" label="Horamètre actuel"/>
-        <NumberInput source="entretien" label="Prochain entretien"/>
-        <NumberInput source="changementMoteur" label="Changement du moteur" />
-        <NumberInput source="seuilAlerte" label="Seuil d'alerte (en h) avant entretien"/>
-        <NumberInput source="seuilAlerteChangementMoteur" label="Seuil d'alerte (en h) avant changement du moteur"/>
+  const { client } = useClient();
+  
+    const MicrotrakInput = () => {
+      return !clientWithMicrotrakTags(client) ? null : 
         <TextInput source="codeBalise" label="Code Microtrak"/>
-        <BooleanInput source="decimal" label="Horamètre décimal"/>
-      </SimpleForm>
-  </Edit>
+    };
+
+  return (
+    <Edit >
+        <SimpleForm>
+          <TextInput source="immatriculation" label="Immatriculation"/>
+          <NumberInput source="horametre" label="Horamètre actuel"/>
+          <NumberInput source="entretien" label="Prochain entretien"/>
+          <NumberInput source="changementMoteur" label="Changement du moteur" />
+          <NumberInput source="seuilAlerte" label="Seuil d'alerte (en h) avant entretien"/>
+          <NumberInput source="seuilAlerteChangementMoteur" label="Seuil d'alerte (en h) avant changement du moteur"/>
+          <MicrotrakInput/>
+          <BooleanInput source="decimal" label="Horamètre décimal"/>
+        </SimpleForm>
+    </Edit>
   )
 };
